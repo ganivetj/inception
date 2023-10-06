026b10921240c4442c6884626dd64b1067550d52
Julien Ganivet <jganivet@MacBookPro.local>
2023-10-06 16:28:11 +0200

    1628

--- srcs/mariadb/init-db.sh
+++ srcs/mariadb/init-db.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
-mysqld_safe &
-sleep 3s
-
 if [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASS" ]; then
   echo "Undefined environment variables"
   exit 1
 fi
+
+mysqld_safe &
+
+until mysqladmin ping >/dev/null 2>&1; do
+  echo "Waiting for database to come up..."
+  sleep 1
+done
 
 mysql -e "CREATE DATABASE $DB_NAME;"
 mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_PASS';"
